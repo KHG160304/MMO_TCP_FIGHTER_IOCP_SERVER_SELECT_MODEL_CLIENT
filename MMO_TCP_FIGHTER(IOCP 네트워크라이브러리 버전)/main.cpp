bc924e1c6180686779897ff932b97e72954c5a21
khg <gusrn160304@gmail.com>
2023-06-12 03:41:58 +0900

    feat!: main.cpp 추가
    1. 메인스레드에서 실행된다.
    2. 기본적인 라이브러리 이니셜라이저와, 키보드 입력을 받고, 모니터링도 진행한다.
    3. 모든 스레드가 종료된 후에 메인 스레드가 종료된다.

---
+++ MMO_TCP_FIGHTER(IOCP 네트워크라이브러리 버전)/main.cpp
@@ -0,0 +1,120 @@
+﻿//#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include "Network.h"
+#include "RingBuffer.h"
+#include "ProcessContentsPacket.h"
+#include "Log.h"
+#include "Monitoring.h"
+
+#define SERVERPORT	6000
+#define IOCP_WORKER_THREAD_CREATE_CNT	0
+#define IOCP_WORKER_THREAD_RUNNING_CNT	0
+
+bool isRunning = true;
+
+int main(void)
+{
+	int key;
+	if (!InitTCPFighterContentThread())
+	{
+		return 0;
+	}
+
+	if (!InitNetworkLib(SERVERPORT, IOCP_WORKER_THREAD_CREATE_CNT, IOCP_WORKER_THREAD_RUNNING_CNT))
+	{
+		ExitTCPFighterContentThread();
+		return 0;
+	}
+
+	SetCharacterCntHandle(GetCharacterCnt);
+	SetSessionCntHandle(GetCurrentSessionCnt);
+	SetSectorCharacterCntHandle(GetSectorCharacterCnt);
+	//SetDisconnectFromServerCntHandle();
+
+	while (isRunning)
+	{
+		if (_kbhit())
+		{
+			key = _getch();
+			if (key == 'Q' || key == 'q')
+			{
+				isRunning = false;
+			}
+		}
+
+		Monitoring();
+	}
+
+	RequestExitNetworkLibThread();
+	ExitTCPFighterContentThread();
+
+	return 0;
+}
+
+
+/*unsigned WINAPI UpdateThread(LPVOID args)
+{
+	int useSize;
+	EchoMessage message;
+	DWORD result;
+	HANDLE arrEvent[2] = { hEventWakeUpdateThread, hEventExitUpdateThread };
+	_Log(dfLOG_LEVEL_SYSTEM, "Start UpdateThread");
+	for (;;)
+	{
+		// Update 스레드를 멀티스레드로 돌려보기 위한 코드조각
+		//result = WaitForMultipleObjects(2, arrEvent, FALSE, INFINITE);
+		//if (result == WAIT_FAILED)
+		//{
+		//	_Log(dfLOG_LEVEL_SYSTEM, "Event WAIT_FAILED error code: %d", GetLastError());
+		//	_Log(dfLOG_LEVEL_SYSTEM, "Exit UpdateThread");
+		//	return -1;
+		//}
+		//else if (result == (WAIT_OBJECT_0 + 1))
+		//{
+		//	_Log(dfLOG_LEVEL_SYSTEM, "Exit UpdateThread");
+		//	return 0;
+		//}
+
+		//while (updateThreadQueue.GetUseSize())
+		//{
+		//	AcquireSRWLockExclusive(&updateThreadQueueLock);
+		//	if (!updateThreadQueue.Dequeue((char*)&message, sizeof(message)))
+		//	{
+		//		ReleaseSRWLockExclusive(&updateThreadQueueLock);
+		//		break;
+		//	}
+		//	SendPacket(message.sessionID, *(message.ptrPayload));
+		//	ReleaseSRWLockExclusive(&updateThreadQueueLock);
+		//	delete message.ptrPayload;
+		//}
+
+		
+		//	아래는 Update 스레드가 싱글스레드에서 작동하는 코드이다.
+		
+		if (!(useSize = updateThreadQueue.GetUseSize()))
+		{
+			result = WaitForMultipleObjects(2, arrEvent, FALSE, INFINITE);
+			if (result == WAIT_FAILED)
+			{
+				_Log(dfLOG_LEVEL_SYSTEM, "Event WAIT_FAILED error code: %d", GetLastError());
+				_Log(dfLOG_LEVEL_SYSTEM, "Exit UpdateThread");
+				return -1;
+			}
+			else if (result == (WAIT_OBJECT_0 + 1))
+			{
+				_Log(dfLOG_LEVEL_SYSTEM, "Exit UpdateThread");
+				return 0;
+			}
+			useSize = updateThreadQueue.GetUseSize();
+		}
+
+		while (useSize)
+		{
+			updateThreadQueue.Dequeue((char*)&message, sizeof(message));
+			SendPacket(message.sessionID, *(message.ptrPayload));
+			delete message.ptrPayload;
+			useSize -= sizeof(EchoMessage);
+		}
+	}
+}*/
