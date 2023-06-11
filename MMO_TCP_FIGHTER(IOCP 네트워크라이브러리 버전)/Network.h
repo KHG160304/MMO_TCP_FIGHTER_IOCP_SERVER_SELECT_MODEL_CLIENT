eb2e2e74a42fd44e66dc04091ef7e8e0a9b22b32
khg <gusrn160304@gmail.com>
2023-06-11 15:48:27 +0900

    feat!: Network.h 추가
    1. SetOnRecvEvent
    2. SetOnAcceptEvent
    3. SetOnDisconnectEvent
    4. SendPacket
    5. SetOnRecvBeforeEvent
    함수 추가

---
+++ MMO_TCP_FIGHTER(IOCP 네트워크라이브러리 버전)/Network.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#ifndef __NETWORK_H__
+#define	__NETWORK_H__
+#include "SerializationBuffer.h"
+#define WINAPI	__stdcall
+
+typedef unsigned short		WORD;
+typedef unsigned long		DWORD;
+typedef	void*				LPVOID;
+typedef unsigned long long	SESSIONID;
+
+void SetOnRecvBeforeEvent(void (*_OnRecvBefore)(SESSIONID sessionID));
+void SetOnRecvEvent(void (*_OnRecv)(SESSIONID sessionID, SerializationBuffer& packet));
+void SetOnAcceptEvent(void (*_OnAccept)(SESSIONID sessionID));
+void SetOnDisconnectEvent(void (*_OnDisconnect)(SESSIONID sessionID));
+void SendPacket(SESSIONID sessionID, const SerializationBuffer& sendPacket);
+
+void DisconnectSession(SESSIONID sessionID);
+
+size_t GetAcceptTotalCnt();
+size_t GetCurrentSessionCnt();
+
+void RequestExitNetworkLibThread(void);
+bool InitNetworkLib(WORD port, DWORD createIOCPWorkerThreadCnt = 0, DWORD runningIOCPWorkerThreadCnt = 0);
+
+#endif // !__NETWORK_H__
