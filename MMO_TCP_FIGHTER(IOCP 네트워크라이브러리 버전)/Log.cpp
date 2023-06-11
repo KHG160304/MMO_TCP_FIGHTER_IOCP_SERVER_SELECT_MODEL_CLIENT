aa481072f075825fcbcf17981fb70eb4a63fb80d
khg <gusrn160304@gmail.com>
2023-06-11 15:42:09 +0900

    feat!: Log 찍는 함수 추가
    1. __declspec(thread) 키워드를 이용해서, 스레드 별로 로그 버퍼가 생성되도록 수정(TLS)

---
+++ MMO_TCP_FIGHTER(IOCP 네트워크라이브러리 버전)/Log.cpp
@@ -0,0 +1,10 @@
+#include "Log.h"
+
+int __gLogLvl = dfLOG_LEVEL_SYSTEM;
+__declspec(thread) wchar_t __gLogBuffer[2048 + 1];
+const wchar_t* __strLogLvl[3] = { L"[DEBUG]", L"[ERROR]", L"[SYSTEM]" };
+
+void Log(const wchar_t* strLog, int logLvl)
+{
+	wprintf_s(L"%s\n", strLog);
+}
---
+++ MMO_TCP_FIGHTER(IOCP 네트워크라이브러리 버전)/Log.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#include <strsafe.h>
+#include <time.h>
+
+#define	dfLOG_LEVEL_DEBUG	0
+#define dfLOG_LEVEL_ERROR	1
+#define dfLOG_LEVEL_SYSTEM	2
+
+#define __FILENAME__	strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__
+
+#define _Log(logLvl, fmt, ...)											\
+do {																	\
+	tm timeInfo;														\
+	time_t timestamp;													\
+	if (logLvl >= __gLogLvl)											\
+	{																	\
+		timestamp = time(nullptr);										\
+		localtime_s(&timeInfo, &timestamp);								\
+		StringCchPrintf(__gLogBuffer, 2048								\
+			, L"%-8s [%02d/%02d/%04d %02d:%02d:%02d] [%hs:%hs:%d] " fmt	\
+			, __strLogLvl[logLvl]										\
+			, timeInfo.tm_mon + 1										\
+			, timeInfo.tm_mday											\
+			, timeInfo.tm_year + 1900									\
+			, timeInfo.tm_hour											\
+			, timeInfo.tm_min											\
+			, timeInfo.tm_sec											\
+			, __FILENAME__												\
+			, __func__													\
+			, __LINE__													\
+			, ##__VA_ARGS__);											\
+		Log(__gLogBuffer, logLvl);										\
+	}\
+} while (0)
+
+extern int __gLogLvl;
+extern __declspec(thread) wchar_t __gLogBuffer[2048 + 1];
+extern const wchar_t* __strLogLvl[3];
+
+void Log(const wchar_t* strLog, int logLvl);
+#endif
